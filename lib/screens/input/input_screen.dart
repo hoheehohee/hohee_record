6d8d0af67db1f38984ab9e4b6a071b4b7e612479
hoheehohee <www.hohee@gmail.com>
2022-05-02 16:04:14 +0900

    [feat]: price input field

--- lib/screens/input/input_screen.dart
+++ lib/screens/input/input_screen.dart
@@ -1,4 +1,5 @@
 import 'package:beamer/beamer.dart';
+import 'package:extended_image/extended_image.dart';
 import 'package:flutter/material.dart';
 import 'package:hohee_record/constants/common_size.dart';
 import 'package:hohee_record/widgets/multi_image_select.dart';
@@ -14,6 +15,8 @@ class InputScreen extends StatefulWidget {
 }
 
 class _InputScreenState extends State<InputScreen> {
+  bool _suggestPriceSelected = false;
+
   final _divider = Divider(
     height: 1,
     color: Colors.grey[350],
@@ -44,19 +47,85 @@ class _InputScreenState extends State<InputScreen> {
           children: [
             const MultiImageSelect(),
             _divider,
-            TextFormField(
-              decoration: const InputDecoration(
-                hintText: '글 제목',
-                contentPadding: EdgeInsets.symmetric(horizontal: common_padding),
-                border: UnderlineInputBorder(
-                  borderSide: BorderSide(color: Colors.transparent),
-                ),
-                enabledBorder: UnderlineInputBorder(
-                  borderSide: BorderSide(color: Colors.transparent),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: common_padding),
+              child: TextFormField(
+                decoration: const InputDecoration(
+                  hintText: '글 제목',
+                  border: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.transparent),
+                  ),
+                  enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.transparent),
+                  ),
                 ),
               ),
             ),
-            _divider
+            _divider,
+            const ListTile(
+              dense: true,
+              title: Text('선택'),
+              trailing: Icon(Icons.navigate_next),
+            ),
+            Row(
+              children: [
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: common_padding),
+                    child: TextFormField(
+                      keyboardType: TextInputType.number,
+                      decoration: InputDecoration(
+                        hintText: '얼마에 파시겠어요?',
+                        prefixIcon: ImageIcon(
+                          const ExtendedAssetImageProvider(
+                              'assets/imgs/won.png'),
+                          size: 20,
+                          color: Colors.grey[350],
+                        ),
+                        prefixIconConstraints:
+                            const BoxConstraints(maxWidth: 20),
+                        contentPadding: const EdgeInsets.symmetric(
+                            vertical: common_sm_padding),
+                        border: const UnderlineInputBorder(
+                          borderSide: BorderSide(color: Colors.transparent),
+                        ),
+                        enabledBorder: const UnderlineInputBorder(
+                          borderSide: BorderSide(color: Colors.transparent),
+                        ),
+                        focusedBorder: const UnderlineInputBorder(
+                          borderSide: BorderSide(color: Colors.transparent),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                TextButton.icon(
+                  onPressed: () {
+                    setState(() {
+                      _suggestPriceSelected = !_suggestPriceSelected;
+                    });
+                  },
+                  icon: Icon(
+                    Icons.check_circle_outline,
+                    color: _suggestPriceSelected
+                        ? Theme.of(context).primaryColor
+                        : Colors.black54,
+                  ),
+                  label: Text(
+                    "가격제한 받기",
+                    style: TextStyle(
+                      color: _suggestPriceSelected
+                          ? Theme.of(context).primaryColor
+                          : Colors.black54,
+                    ),
+                  ),
+                  style: TextButton.styleFrom(
+                    primary: Colors.black45,
+                    backgroundColor: Colors.transparent,
+                  ),
+                )
+              ],
+            ),
           ],
         ));
   }
